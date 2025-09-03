23b3119928fe1f15bf3d6f84da3f29388496d3f1
Priyansu Shakya <priyansushakya9@gmail.com>
2025-09-03 19:19:58 +0545

    Card Generator

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\AuthMiddleware;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\CardController;
@@ -12,8 +13,11 @@ Route::post('/check', [LoginController::class, 'check'])->name('check');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register/store', [RegisterController::class, 'store'])->name('register.store');
 
+// Protected Admin Routes (with middleware)
+Route::middleware([AuthMiddleware::class])->group(function () {
 Route::get('/create', [CardController::class, 'create'])->name('create');
 Route::get('/layout', [CardController::class, 'layout'])->name('layout');
 Route::post('/store', [CardController::class, 'store'])->name('store');
 Route::get('/show/{id}', [CardController::class, 'show'])->name('show');
+});
 
